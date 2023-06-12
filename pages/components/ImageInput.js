6a8efa6c5ef6e0d2cc474a148e4600136c1e8e6a
nicoco <695709347xww@gmail.com>
2023-06-11 21:08:15 -0700

    Refactor the index page and add the ImageInput page and its api

--- pages/components/ImageInput.js
+++ pages/components/ImageInput.js
@@ -1,9 +1,9 @@
 import { useState, useCallback, useEffect } from "react";
 import Editor from "./Editor";
 
-export default function TextInput() {
-  const [result, setResult] = useState("// type a text prompt above and click 'Generate content'");
-  const [textInput, setTextInput] = useState("");
+export default function ImageInput() {
+  const [imageResult, setImageResult] = useState("// type a text prompt above and click 'Generate image'");
+  const [imageInput, setImageInput] = useState("");
   const [waiting, setWaiting] = useState(false);
   const [sandboxRunning, setSandboxRunning] = useState(false);
   const [logMsg, setlogMsg] = useState("");
@@ -40,32 +40,40 @@ export default function TextInput() {
   //   return () => window.removeEventListener("message", handler)
   // }, [result, sandboxRunning])
 
-  function textInputChange(event) {
+  function imageInputChange(event) {
     event.preventDefault();
-    setTextInput(event.target.value);
+    setImageInput(event.target.value);
   }
 
-  async function textInputSubmit(event) {
+  async function imageInputSubmit(event) {
+    console.log('i am in imageInputSubmit')
     event.preventDefault();
     setlogMsg("");
     setWaiting(true);
-    setResult("// Please be patient, this may take a while...");
+    setImageResult("// Please be patient, this may take a while...");
     setSelVal("");
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_REMOTE_API_URL || ''}/api/generateText`, {
+      console.log(`before fetch, prompt ${imageInput}`)
+      const response = await fetch(`${process.env.NEXT_PUBLIC_REMOTE_API_URL || ''}/api/generateImage`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ prompt: textInput, promptType: selVal }),
+        body: JSON.stringify({ prompt: imageInput, promptType: selVal }),
       });
-
+      console.log(`my response ${response}`)
       const data = await response.json();
+      console.log(`my data ${data}`)
       if (response.status !== 200) {
         setWaiting(false);
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
-      setResult(data.code);
+      console.log(typeof(data.code))
+    //   TODO: This is not the best way to extract output
+      const outputRegex = /"output":\s*\["(.*?)"]/;
+      const match = data.code.match(outputRegex);
+      const output = match ? match[1] : null;
+      setImageResult(output);
       setSandboxRunning(true);
       setWaiting(false);
     } catch(error) {
@@ -76,7 +84,7 @@ export default function TextInput() {
   }
 
   const editorChange = useCallback((value, viewUpdate) => {
-    setResult(value);
+    setImageResult(value);
   }, []);
   
   function runClickPlay(event) {
@@ -90,20 +98,20 @@ export default function TextInput() {
     setlogMsg("");
   }
 
-  function textSelectChange(event) {
+  function imageSelectChange(event) {
     setSelVal(event.target.value);
     event.preventDefault();
     const search = event.target.value;
     const selectedEg = egArray.find((obj) => obj.value === search);
     if(selectedEg) {
       setlogMsg('');
-      setTextInput(selectedEg.prompt);
-      setResult(selectedEg.code);
+      setImageInput(selectedEg.prompt);
+      setImageResult(selectedEg.code);
       setSandboxRunning(true);
     } else {
       setlogMsg('');
-      setTextInput('');
-      setResult('');
+      setImageInput('');
+      setImageResult('');
       setSandboxRunning(false);
     }
   }
@@ -111,13 +119,13 @@ export default function TextInput() {
     return (
       <div className="rounded-md border border-gray-100 shadow-md shadow-emerald-600/30 bg-white p-3">
         <div className="flex justify-between xs:mb-2">
-          <h3 className="font-semibold text-gray-500">Text prompt</h3>
+          <h3 className="font-semibold text-gray-500">Image Description prompt</h3>
           <select 
             name="examples" 
             id="eg-select" 
             value={selVal}
             className="bg-emerald-100 rounded text-sm px-1 text-gray-600"
-            onChange={textSelectChange}
+            onChange={imageSelectChange}
             >
               <option value="">Choose a content type</option>
               {contentLengthArray?.map((eg, index) => {
@@ -126,24 +134,24 @@ export default function TextInput() {
               )}
           </select>
         </div>
-        <form onSubmit={textInputSubmit} className="w-full">
+        <form onSubmit={imageInputSubmit} className="w-full">
           <textarea key="textarea-01" className="block min-h-[50px] xs:min-h-[70px] border-[1.5px] border-emerald-500 p-2 rounded w-full mb-2 text-sm
           disabled:border-gray-300 disabled:text-gray-600 disabled:bg-gray-100"
             type="text"
             name="prompt"
-            placeholder="Enter a text prompt for content generation. Click the dropdown for type of content."
-            value={textInput}
-            onChange={textInputChange}
+            placeholder="Describe your image."
+            value={imageInput}
+            onChange={imageInputChange}
             disabled={waiting}
           />
-          <Editor key="editor-01" result={result} onChange={editorChange} waiting={waiting}/>
+          <Editor key="editor-01" result={imageResult} onChange={editorChange} waiting={waiting}/>
           { waiting ? 
           <button className="bg-gray-300 p-2 rounded w-full text-white text-sm px-3" type="submit" disabled>
             <img src="loading.png" alt="loading icon" className="animate-spin w-4 h-4 mr-2 inline" />
-            Generating content...
+            Generating image...
           </button>
           : 
-          <button className="bg-emerald-500 p-2 rounded w-full text-white text-sm px-3 cursor-pointer" type="submit">Generate content</button> }
+          <button className="bg-emerald-500 p-2 rounded w-full text-white text-sm px-3 cursor-pointer" type="submit">Generate image</button> }
           
         </form>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,113 +1,114 @@
 import Head from "next/head";
 import { useState, useCallback, useEffect } from "react";
 import TextInput from "./components/TextInput";
+import ImageInput from "./components/ImageInput";
 import Editor from "./components/Editor";
 
 export default function Home() {
-  const [result, setResult] = useState("// type a text prompt above and click 'Generate content'");
-  const [textInput, setTextInput] = useState("");
-  const [waiting, setWaiting] = useState(false);
-  const [sandboxRunning, setSandboxRunning] = useState(false);
-  const [logMsg, setlogMsg] = useState("");
-  const [selVal, setSelVal] = useState("");
+  // const [result, setResult] = useState("// type a text prompt above and click 'Generate content'");
+  // const [textInput, setTextInput] = useState("");
+  // const [waiting, setWaiting] = useState(false);
+  // const [sandboxRunning, setSandboxRunning] = useState(false);
+  // const [logMsg, setlogMsg] = useState("");
+  // const [selVal, setSelVal] = useState("");
 
-  const contentLengthArray = [
-    {
-      value: "30 Characters Headline",
-      length: "30"
-    },
-    {
-      value: "90 Characters Description",
-      length: "90"
-    }
-  ]
+  // const contentLengthArray = [
+  //   {
+  //     value: "30 Characters Headline",
+  //     length: "30"
+  //   },
+  //   {
+  //     value: "90 Characters Description",
+  //     length: "90"
+  //   }
+  // ]
 
-  useEffect(() => {
-    let ranOnce = false;
+  // useEffect(() => {
+  //   let ranOnce = false;
 
-    const handler = event => {
-      const data = JSON.parse(event.data)
-      if (!ranOnce) {
-        setlogMsg(data.logMsg);
-        ranOnce = true;
-      } else {
-        setlogMsg(msg => msg + '\n' + data.logMsg);
-      }
-    }
+  //   const handler = event => {
+  //     const data = JSON.parse(event.data)
+  //     if (!ranOnce) {
+  //       setlogMsg(data.logMsg);
+  //       ranOnce = true;
+  //     } else {
+  //       setlogMsg(msg => msg + '\n' + data.logMsg);
+  //     }
+  //   }
 
-    window.addEventListener("message", handler)
+  //   window.addEventListener("message", handler)
 
-    // clean up
-    return () => window.removeEventListener("message", handler)
-  }, [result, sandboxRunning])
+  //   // clean up
+  //   return () => window.removeEventListener("message", handler)
+  // }, [result, sandboxRunning])
 
-  function textInputChange(event) {
-    event.preventDefault();
-    setTextInput(event.target.value);
-  }
+  // function textInputChange(event) {
+  //   event.preventDefault();
+  //   setTextInput(event.target.value);
+  // }
 
-  async function textInputSubmit(event) {
-    event.preventDefault();
-    setlogMsg("");
-    setWaiting(true);
-    setResult("// Please be patient, this may take a while...");
-    setSelVal("");
-    try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_REMOTE_API_URL || ''}/api/generate`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ prompt: textInput, promptType: selVal }),
-      });
+  // async function textInputSubmit(event) {
+  //   event.preventDefault();
+  //   setlogMsg("");
+  //   setWaiting(true);
+  //   setResult("// Please be patient, this may take a while...");
+  //   setSelVal("");
+  //   try {
+  //     const response = await fetch(`${process.env.NEXT_PUBLIC_REMOTE_API_URL || ''}/api/generate`, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ prompt: textInput, promptType: selVal }),
+  //     });
 
-      const data = await response.json();
-      if (response.status !== 200) {
-        setWaiting(false);
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
-      setResult(data.code);
-      setSandboxRunning(true);
-      setWaiting(false);
-    } catch(error) {
-      console.error(error);
-      alert(error.message);
-      setWaiting(false);
-    }
-  }
+  //     const data = await response.json();
+  //     if (response.status !== 200) {
+  //       setWaiting(false);
+  //       throw data.error || new Error(`Request failed with status ${response.status}`);
+  //     }
+  //     setResult(data.code);
+  //     setSandboxRunning(true);
+  //     setWaiting(false);
+  //   } catch(error) {
+  //     console.error(error);
+  //     alert(error.message);
+  //     setWaiting(false);
+  //   }
+  // }
 
-  const editorChange = useCallback((value, viewUpdate) => {
-    setResult(value);
-  }, []);
+  // const editorChange = useCallback((value, viewUpdate) => {
+  //   setResult(value);
+  // }, []);
   
-  function runClickPlay(event) {
-    event.preventDefault();
-    setSandboxRunning(true);
-  }
+  // function runClickPlay(event) {
+  //   event.preventDefault();
+  //   setSandboxRunning(true);
+  // }
 
-  function runClickStop(event) {
-    event.preventDefault();
-    setSandboxRunning(false);
-    setlogMsg("");
-  }
+  // function runClickStop(event) {
+  //   event.preventDefault();
+  //   setSandboxRunning(false);
+  //   setlogMsg("");
+  // }
 
-  function textSelectChange(event) {
-    setSelVal(event.target.value);
-    event.preventDefault();
-    const search = event.target.value;
-    const selectedEg = egArray.find((obj) => obj.value === search);
-    if(selectedEg) {
-      setlogMsg('');
-      setTextInput(selectedEg.prompt);
-      setResult(selectedEg.code);
-      setSandboxRunning(true);
-    } else {
-      setlogMsg('');
-      setTextInput('');
-      setResult('');
-      setSandboxRunning(false);
-    }
-  }
+  // function textSelectChange(event) {
+  //   setSelVal(event.target.value);
+  //   event.preventDefault();
+  //   const search = event.target.value;
+  //   const selectedEg = egArray.find((obj) => obj.value === search);
+  //   if(selectedEg) {
+  //     setlogMsg('');
+  //     setTextInput(selectedEg.prompt);
+  //     setResult(selectedEg.code);
+  //     setSandboxRunning(true);
+  //   } else {
+  //     setlogMsg('');
+  //     setTextInput('');
+  //     setResult('');
+  //     setSandboxRunning(false);
+  //   }
+  // }
 
   return (
     <>
@@ -130,8 +131,9 @@ export default function Home() {
         </header>
         <div className="flex flex-col gap-4 2xl:flex-row w-full">
           <div className="flex flex-col gap-4 2xl:w-1/2">
-            <TextInput key="textinput-01" textInput={textInput} onChange={textInputChange} onSubmit={textInputSubmit} waiting={waiting} selectVal={selVal} selectChange={textSelectChange} egArray={contentLengthArray}/>
-            <Editor key="editor-01" result={result} onChange={editorChange} waiting={waiting}/>
+            <TextInput/>
+            <br/>
+            <ImageInput/>
           </div>
         </div>
         <p className="text-gray-400 text-sm text-center mt-3">Made by <a href="https://chat.openai.com/" target="_blank" className="underline">AICG</a></p>
