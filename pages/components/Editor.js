4d88ecd9cb59117e0e6c0a7ec8447224bb82fc97
nicoco <695709347xww@gmail.com>
2023-07-22 18:11:02 -0700

    Remove the download button

--- pages/components/Editor.js
+++ pages/components/Editor.js
@@ -5,6 +5,7 @@ export default function Editor({result, onChange, waiting}) {
   const fileUrl = result 
   var isDisabled = !fileUrl || !fileUrl.endsWith(".png");
 
+  // // [Deprecated] - Download png file from the returned url using Stable Diffusion API --------------------------
   // The below is to open up the image in a new tab
   // const handleDownload = () => {
   //   const newTab = window.open(fileUrl, "_blank");
@@ -15,29 +16,29 @@ export default function Editor({result, onChange, waiting}) {
   //   }
   // };
 
-  // The below is to actually download to user's local file system
-  const handleDownload = async () => {
-    try {
-      const response = await fetch(fileUrl);
-      const blob = await response.blob();
+  // // The below is to actually download to user's local file system
+  // const handleDownload = async () => {
+  //   try {
+  //     const response = await fetch(fileUrl);
+  //     const blob = await response.blob();
 
-      // Create a temporary URL object
-      const url = window.URL.createObjectURL(blob);
+  //     // Create a temporary URL object
+  //     const url = window.URL.createObjectURL(blob);
 
-      // Create a temporary <a> element
-      const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", "image.png");
-      link.click();
+  //     // Create a temporary <a> element
+  //     const link = document.createElement("a");
+  //     link.href = url;
+  //     link.setAttribute("download", "image.png");
+  //     link.click();
 
-      // Cleanup
-      URL.revokeObjectURL(url);
+  //     // Cleanup
+  //     URL.revokeObjectURL(url);
 
-      setDownloaded(true);
-    } catch (error) {
-      console.error("Error downloading file:", error);
-    }
-  }
+  //     setDownloaded(true);
+  //   } catch (error) {
+  //     console.error("Error downloading file:", error);
+  //   }
+  // }
 
   return (
     <div className="max-h-[25vh] overflow-scroll rounded-md border border-gray-100 shadow-md shadow-emerald-600/30 bg-white p-3">
@@ -53,7 +54,7 @@ export default function Editor({result, onChange, waiting}) {
         readOnly={waiting}
       />
 
-    <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
+    {/* <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
       {fileUrl && (
         <button
           onClick={handleDownload}
@@ -66,7 +67,7 @@ export default function Editor({result, onChange, waiting}) {
           Download File
         </button>
       )}
-    </div>
+    </div> */}
 
     </div>
   );
