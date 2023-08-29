9871cda5bd91fb81bbcdc75f98f679f1906f25e5
Kerby Chang <kerbychang@gmail.com>
2023-08-29 01:09:10 -0700

    fix: Editor text size and height

--- pages/components/Editor.js
+++ pages/components/Editor.js
@@ -4,7 +4,7 @@ export default function Editor({result, onChange, waiting}) {
   const fileUrl = result
 
   return (
-    <div className="max-h-[50vh] min-h-[25vh] rounded-md border border-gray-100 shadow-md shadow-emerald-600/30 bg-white p-3" style={{ padding: "10px" }}>
+    <div className="max-h-[50vh] min-h-[50vh] rounded-md border border-gray-100 shadow-md shadow-emerald-600/30 bg-white p-3" style={{ padding: "10px" }}>
       <h3 className="font-semibold text-gray-500 mb-2">Content</h3>
       <Container className = "output-container">
         <Card className = "output-card overflow-scroll"
